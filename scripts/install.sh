7f49275f5cf7730d8968777cffd4d9557688d8e5
Anon4You <alienkrishn@gmail.com>
2025-01-18 01:49:16 +0530

    first release of tsfconsole

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,13 +5,16 @@ c1="\e[31m"
 c2="\e[32m"
 c0="\e[0m"
 
+if [[ -d $tsfDir ]]; then
+  rm -rf tsfDir > /dev/null 2>&1
+fi
 printf "$c2 --Updating repositoris...$c0\n"
 apt update -y
 
 printf "$c2 --Installing required packages...$c0\n"
 apt install git curl python build-essential cmake ninja libopenblas libandroid-execinfo patchelf binutils-is-llvm rust jq python-pillow nodejs qpdf libiconv libxml2 libxslt -y
 
-apt cleane
+apt clean
 
 git clone --quiet https://github.com/Anon4You/termsploit-framework $tsfDir
 
